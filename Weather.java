ffd4a53ee1fccb4d04974e8c5e70fdfd2b8df8fb
Nabil Siddiqui <47227453+nabeelsid@users.noreply.github.com>
2020-04-30 14:45:48 -0400

    Remove System Tray Icon Route
    - Changing route to display current weather information in system tray to a GUI using swing in Java.
    - Maybe add System Tray icon later

--- Weather.java
+++ Weather.java
@@ -1,5 +1,5 @@
-package weather;
-
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -7,24 +7,50 @@ import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import java.net.URL;
 import java.net.URLConnection;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
-public class Weather {
+public class weather {
+	
+	public weather() {
+		JFrame frame = new JFrame(); //First create this object 
+		JPanel panel = new JPanel(); //panel goes inside the frame
+		JButton button = new JButton("Find Weather");
+		JLabel label =  new JLabel("Get Weather");
+		panel.setBorder(BorderFactory.createEmptyBorder(50, 150, 0, 150));
+		panel.setLayout(new GridLayout(2,2));
+		panel.add(button);
+		
+		
+		
+		frame.add(panel, BorderLayout.CENTER); //here we add the panel in the frame with border as Center
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Weather GUI");
+		frame.pack();
+		frame.setVisible(true);
+	}
 
 	public static String cityCall() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Please Enter a City : ");
 		String cityInput = input.next().toLowerCase().trim();
-		//input.close();
+		// input.close();
 		return cityInput;
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked", "resource" })
 	public static void main(String[] args) {
+		weather myWeather = new weather();
 		try {
 			String cityInput = cityCall();
 			while (cityInput != "exit") {
@@ -66,9 +92,8 @@ public class Weather {
 				}
 			}
 			
-
 		} catch (FileNotFoundException e) {
-			//System.out.println("Invalid argument. No such city");
+			// System.out.println("Invalid argument. No such city");
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ParseException e) {
